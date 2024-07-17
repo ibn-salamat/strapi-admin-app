c418c1d0c45e9f44cfff7159b87cd0a1a4466203
nurlykhan <nurlykhan.salamatuly@samgau.com>
2024-07-18 01:10:12 +0500

    feat: get products

--- front/src/pages/admin/admin.tsx
+++ front/src/pages/admin/admin.tsx
@@ -1,18 +1,21 @@
-import { BASE_URL, STRAPI_API_TOKEN } from "@/src/config"
+import type { Product } from "@/src/types/product"
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { List, ListItem } from "@mui/material"
+import { BASE_URL, STRAPI_API_TOKEN } from "@/src/config"
 
 export const Admin = () => {
+    const [products, setProducts] = useState<{attributes: Product}[]>([])
 
     const getProducts = async () => {
         try {
-            await axios.get(`${BASE_URL}/api/products`, {
-                
+            const response =  await axios.get(`${BASE_URL}/api/products`, {
                 headers: {
                     Authorization: `Bearer ${STRAPI_API_TOKEN}`
                 }
             })
-    
+
+            setProducts(response.data.data)
             } catch (error) {
                 console.log(error)
             }
@@ -22,5 +25,22 @@ export const Admin = () => {
         getProducts()
     }, [])
 
-    return <h1>Admin</h1>
+
+    return <>
+        <h1>Admin</h1>
+
+
+        <List>
+        {products.map((el) => {
+            const { attributes } = el;
+
+            return <ListItem>
+                product_id: {attributes.product_id} <br />
+                title: {attributes.title} <br />
+                price: {attributes.price}<br />
+
+            </ListItem>
+        })}
+        </List>
+    </>
 }
---
+++ front/src/types/product.ts
@@ -0,0 +1,6 @@
+export type Product = {
+    createdAt: string,
+    price: number,
+    title: string,
+    product_id: string
+}

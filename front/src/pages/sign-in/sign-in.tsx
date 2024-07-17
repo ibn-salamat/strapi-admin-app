365eba17fccea1c35f81732f0018b7137b27e2b7
nurlykhan <nurlykhan.salamatuly@samgau.com>
2024-07-17 18:24:24 +0500

    feat: style sign-in

--- front/src/pages/sign-in/sign-in.tsx
+++ front/src/pages/sign-in/sign-in.tsx
@@ -1,3 +1,51 @@
+import {
+  TextField,
+  FormControl,
+  Typography,
+  Card,
+  Button,
+  Box,
+} from "@mui/material"
+
 export const SignIn = () => {
-    return null
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <Card style={{ minWidth: 275, maxWidth: 600, width: "100%", padding: 20 }}>
+          <Typography variant="h4">Sign in</Typography>
+
+        <Box>
+          <FormControl style={{marginTop: 15, width: "100%"}}>
+            <TextField 
+                label="Email"
+                variant="outlined"
+                // helperText="Error" 
+                // error
+            />
+          </FormControl>
+        </Box>
+
+        <Box>
+          <FormControl style={{marginTop: 15, width: "100%"}}>
+            <TextField 
+                type="password" 
+                variant="outlined"
+                label="Password"  
+                // helperText="Error" 
+                // error 
+            />
+          </FormControl>
+        </Box>
+
+        <Box style={{marginTop: 15, display: "flex", gap: 15}}>
+            <Button size="small" color="primary" variant="contained">Sign in</Button>
+            <Button size="small" color="primary">Sign up</Button>
+        </Box>
+      </Card>
+    </Box>
+  )
 }
--- front/src/router.tsx
+++ front/src/router.tsx
@@ -1,8 +1,17 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import { SignIn, SignUp } from "./pages";
 
 export const router = createBrowserRouter([
     {
-      path: "/",
-      element: <div>Hello world!</div>,
+      path: "/sign-up",
+      element: <SignUp />,
+    },
+    {
+      path: "/sign-in",
+      element: <SignIn />,
+    },
+    {
+      path: "*",
+      element: <Navigate to="sign-in" />,
     },
 ]);

14d2e09890f38196b170636f99e9b88c1b459b83
nurlykhan <nurlykhan.salamatuly@samgau.com>
2024-07-17 18:49:54 +0500

    feat: validation

--- front/src/pages/sign-in/sign-in.tsx
+++ front/src/pages/sign-in/sign-in.tsx
@@ -6,8 +6,19 @@ import {
   Button,
   Box,
 } from "@mui/material"
+import { useForm } from "react-hook-form"
+
+type FormValues = {
+    email: string
+    password: string
+}
 
 export const SignIn = () => {
+    const { register, handleSubmit, formState: { errors }} = useForm<FormValues>();
+    const onSubmit = (data: FormValues) => {
+        console.log(data)
+    }
+
   return (
     <Box
       display="flex"
@@ -18,13 +29,17 @@ export const SignIn = () => {
       <Card style={{ minWidth: 275, maxWidth: 600, width: "100%", padding: 20 }}>
           <Typography variant="h4">Sign in</Typography>
 
+        <form onSubmit={handleSubmit(onSubmit)}>
         <Box>
           <FormControl style={{marginTop: 15, width: "100%"}}>
             <TextField 
                 label="Email"
                 variant="outlined"
-                // helperText="Error" 
-                // error
+                {...register("email", {
+                    required: true
+                })}
+                helperText={errors.email && "email is required"}
+                error={Boolean(errors.email)}
             />
           </FormControl>
         </Box>
@@ -35,16 +50,20 @@ export const SignIn = () => {
                 type="password" 
                 variant="outlined"
                 label="Password"  
-                // helperText="Error" 
-                // error 
+                {...register("password", {
+                    required: true
+                })}
+                helperText={errors.password && "password is required" }
+                error={Boolean(errors.password)}
             />
           </FormControl>
         </Box>
 
         <Box style={{marginTop: 15, display: "flex", gap: 15}}>
-            <Button size="small" color="primary" variant="contained">Sign in</Button>
+            <Button size="small" color="primary" variant="contained" type="submit">Sign in</Button>
             <Button size="small" color="primary">Sign up</Button>
         </Box>
+        </form>
       </Card>
     </Box>
   )
